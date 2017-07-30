eb2aa06f911ea2adecbf47c8bc528d48863f27d8
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-07-30 11:37:34 -0600

    regex almost working

--- data-raw/load.R
+++ data-raw/load.R
@@ -3,31 +3,64 @@ library(stringr)
 library(pdftools)
 
 page_lines <- function(page) {
-  page %>% str_split('\n') %>% flatten()
+  page %>%
+    str_split('\n') %>%
+    unlist() %>%
+    str_subset('^[:alpha:]')
 }
+
+  # (R[:digit:]{5})               # roster id
+fields_regex <- stringr::regex("
+  (^[A-Za-z\\ {1}]+?)           # campus
+  [\\ ]+                        # whitespace
+  (\\S+)                        # roster id
+  [\\ ]+                        # whitespace
+  ([A-Z\\ \\/{1}]+?(?=\\ \\ ))  # job family, with lookaround
+  [\\ ]+                        # whitespace
+  ([A-Z\\ {1}]+?(?=\\ \\ ))     # job title, with lookaround
+  [\\ ]+                        # whitespace
+  ([A-Z\\ [:punct:]{1}]+?)      # department
+  [\\ ]+                        # whitespace
+  ([[:digit:]\\.]{1,5})         # percent
+  [\\ ]+                        # whitespace
+  (\\$[[:digit:],{1,}]+)$       # funding
+  ", comments = TRUE)  
 
 line_fields <- function(line) {
-  tibble(
-    campus = str_sub(line, 1, 12) %>% str_trim(),
-    roster_id = str_sub(line, 13, 22) %>% str_trim(),
-    job_family = str_sub(line, 23, 55) %>% str_trim(),
-    job_title = str_sub(line, 56, 122) %>% str_trim(),
-    perc = str_sub(line, 123, 125) %>% str_trim(),
-    funding = str_sub(line, 126, 150) %>%
-      str_trim() %>%
-      str_replace('\\$', '') %>%
-      str_replace(',', '')
-  )
+  fields <- str_match(line, fields_regex)
+ 
+  funding <- fields[8] %>%
+    str_replace("\\$", "") %>%
+    str_replace(",", "")
+                
+  tibble(campus     = fields[2],
+         roster.id  = fields[3],
+         job.family = fields[4],
+         job.title  = fields[5],
+         dept       = fields[6],
+         percent    = fields[7],
+         funding    = funding)
 }
-
+  
 txt <- pdf_text('http://www.cu.edu/doc/fy17-personnel-cu-roster-all-campuses.pdf')
 
-cu_personnel <- txt %>%
-  map(page_lines) %>%
-  map_df(line_fields) %>%
-  filter(campus != '' & campus != 'CAMPUS') %>%
-  mutate(campus = as.factor(campus),
-         perc = as.integer(perc),
-         funding = as.integer(funding))
+pages <- txt %>% map(page_lines)
 
-devtools::use_data(cu_personnel)
+# first line from select pages
+page1   <- pages[[1]][[1]]   # CU Boulder
+page200 <- pages[[200]][[1]] # CU Denver Anschutz
+page295 <- pages[[295]][[1]] # UCCS
+
+page1 %>% line_fields
+page200 %>% line_fields
+page295 %>% line_fields
+
+# cu_personnel <- txt %>%
+#   map(page_lines) %>%
+#   map_df(line_fields) %>%
+#   filter(campus != '' & campus != 'CAMPUS') %>%
+#   mutate(campus = as.factor(campus),
+#          perc = as.numeric(perc),
+#          funding = as.numeric(funding))
+# 
+# devtools::use_data(cu_personnel)
