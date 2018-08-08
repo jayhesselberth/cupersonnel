81831f13923801b79807e47d225987864332c55b
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-08-08 17:06:50 -0600

    simplify loading strategy

--- data-raw/load.R
+++ data-raw/load.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(stringr)
 library(pdftools)
 
 page_lines <- function(page) {
@@ -9,39 +8,24 @@ page_lines <- function(page) {
     str_subset('^[:alpha:]')
 }
 
-fields_regex <- stringr::regex("
-  (^[A-Za-z\\ {1}]+?)           # campus
-  [\\ ]+                        # whitespace
-  (\\S{3,7}(?=\\ \\ ))          # roster id
-  [\\ ]+                        # whitespace
-  ([A-Z\\ \\/{1}]+?(?=\\ ))  # job family, with lookaround
-  [\\ ]+                        # whitespace
-  ([A-Z\\ [:punct:][:digit:]]{1,30}(?=\\ ))     # job title, with lookaround
-  [\\ ]+                        # whitespace
-  ([A-Z\\ [:punct:]{1}]+?)      # department
-  [\\ ]+                        # whitespace
-  ([[:digit:]\\.]{1,5})         # percent
-  [\\ ]+                        # whitespace
-  (\\$[[:digit:],{1,}]+)$       # funding
-  ", comments = TRUE)  
-
 line_fields <- function(line) {
-  fields <- str_match(line, fields_regex)
+  fields <- strsplit(line, "\\s{2,}")[[1]]
  
   funding <- fields[8] %>%
     str_replace("\\$", "") %>%
     str_replace(",", "")
                 
-  tibble(campus     = str_trim(fields[2]),
-         roster.id  = str_trim(fields[3]),
-         job.family = str_trim(fields[4]),
-         job.title  = str_trim(fields[5]),
-         dept       = str_trim(fields[6]),
+  tibble(campus     = str_trim(fields[1]),
+         roster.id  = str_trim(fields[2]),
+         dept.group = str_trim(fields[3]),
+         dept       = str_trim(fields[4]),
+         job.family = str_trim(fields[5]),
+         job.title  = str_trim(fields[6]),
          percent    = str_trim(fields[7]),
          funding    = str_trim(funding))
 }
   
-txt <- pdf_text('http://www.cu.edu/doc/fy17-personnel-cu-roster-all-campuses.pdf')
+txt <- pdf_text('https://www.cu.edu/doc/fy18personnelcurosterallcampusespdf')
 
 pages <- txt %>% map(page_lines) %>% unlist()
 
@@ -49,13 +33,15 @@ cu_personnel <- pages %>%
   map_df(line_fields) %>%
   na.omit() %>%
   # filter out a few (<50) incorrectly parsed rows
-  filter(campus != 'CU') %>%
-  filter(campus != 'System') %>%
+  # filter(campus != 'CU') %>%
+  # filter(campus != 'System') %>%
   mutate(campus     = as.factor(campus),
          job.family = as.factor(job.family),
          job.title  = as.factor(job.title),
          dept       = as.factor(dept),
          percent    = as.numeric(percent),
-         funding    = as.numeric(funding))
+         funding    = as.numeric(funding)
+  )
  
 devtools::use_data(cu_personnel, compress = 'xz')
+
