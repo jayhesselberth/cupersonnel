918ccdd469f32603c828c0a2e07504c1f45cc4cc
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-08-08 19:44:13 -0600

    add a vignette

--- R/data.r
+++ R/data.r
@@ -1,16 +1,17 @@
 #' University of Colorado Personnel Data
 #' 
-#' @format A tibble with 23,458 rows and 7 variables:
+#' @format A tibble with 25,314 rows and 8 variables:
 #' \describe{
 #'   \item{campus}{CU campus, a factor}
 #'   \item{roster.id}{a unique ID}
+#'   \item{dept.group}{Department group}
+#'   \item{dept}{department, a factor}
 #'   \item{job.family}{job family, a factor}
 #'   \item{job.title}{job title, a factor}
-#'   \item{dept}{department, a factor}
-#'   \item{percent}{percent }
+#'   \item{percent}{percent}
 #'   \item{funding}{funding} 
 #' }
 #' 
-#' @source \url{http://www.cu.edu/doc/fy17-personnel-cu-roster-all-campuses.pdf}
+#' @source \url{https://www.cu.edu/doc/fy18personnelcurosterallcampusespdf}
 #' 
 "cu_personnel"
